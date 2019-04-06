0db118d0c8251f77386692e9da91d63a705e2613
Pierrick Muller <pierrick.muller@heig-vd.ch>
2019-04-06 11:23:18 +0200

    Tests travis

---
+++ src/test/java/DieTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DieTest {
+    @Test
+    public void DieFirstTest(){
+        int i = 0;
+        assertEquals(0,i);
+    }
+}
