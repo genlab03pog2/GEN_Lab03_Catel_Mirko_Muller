8fd6890a528c5e1026de39cf66dd5592c80ed1f8
Ichimizu <36811556+Ichimizu@users.noreply.github.com>
2019-04-25 16:50:45 +0200

    a player and a game can be created

--- src/main/java/MonopolyGame.java
+++ src/main/java/MonopolyGame.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+
 public class MonopolyGame {
+    private Die die;
+    private Board board;
+    private ArrayList<Player> players;
+
+    // a voir si on fait un constructeur pour init le jeu
+
+    public void setDie(Die die) {
+        this.die = die;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void playGame(){
+
+    }
+
+    public static void main(String args[]){
+        MonopolyGame game = new MonopolyGame();
+        game.setBoard(new Board());
+        game.setDie(new Die());
+
+        int nbPlayers = 4;
+        for(int i = 0; i < nbPlayers; i++) {
+            game.addPlayer(new Player("Player" + i));
+        }
+
+        int turns = 20;
+    }
 }
