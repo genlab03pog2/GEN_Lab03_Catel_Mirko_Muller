acb792af31e4016f66b18b95239a7ff9f2018c47
Ichimizu <36811556+Ichimizu@users.noreply.github.com>
2019-04-25 19:41:42 +0200

    changes on player and game tests

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PlayerTest {
     @Test
     public void newPlayerHasName() {
+        Player.resetPlayers();
         Player player = new Player();
         assertEquals(player.getName(), "Player0");
     }
