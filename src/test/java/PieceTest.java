426fd02eef072cf818b5b91505f54a19214a7231
raph126 <simon.mirkovitch@heig-vd.ch>
2019-05-03 11:16:12 +0200

    Piece tests done, opti import in DieTest

--- src/test/java/PieceTest.java
+++ src/test/java/PieceTest.java
@@ -1,4 +1,19 @@
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PieceTest
 {
+    @ParameterizedTest
+    @ValueSource (ints = {1, 5, 38, 23,39, 0})
+    public void setLocation(int location){
+        Board board = new Board();
+        Piece piece = new Piece();
+
+        piece.setLocation(board.getSquares().get(location));
+        assertEquals(board.getSquares().get(location), piece.getLocation());
+    }
 
 }
