9200629a0ecf118b99800548beef5ec436fa1fe9
raph126 <simon.mirkovitch@heig-vd.ch>
2019-05-03 09:10:28 +0200

    Die: new test

--- src/test/java/DieTest.java
+++ src/test/java/DieTest.java
@@ -1,29 +1,17 @@
 import org.junit.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DieTest {
     @Test
-    public void DieFirstTest(){
-        int i = 0;
-        assertEquals(0,i);
-    }
-
-    @Test
-    public void DieSecondTest(){
-        int i = 1;
-        assertEquals(1,i);
-    }
-
-    @Test
+    @RepeatedTest(1000)
     public void rollGiveTheRandNumberBetweenRange() throws Exception{
         Die die = new Die(6);
         int rolledValue = 0;
-        for(int i = 0; i < 1000; i++){
-            die.roll();
-            rolledValue = die.getFaceValue();
-            assertTrue(1 <= rolledValue && rolledValue <= 6);
-        }
+        die.roll();
+        rolledValue = die.getFaceValue();
+        assertTrue(1 <= rolledValue && rolledValue <= 6);
     }
 }
