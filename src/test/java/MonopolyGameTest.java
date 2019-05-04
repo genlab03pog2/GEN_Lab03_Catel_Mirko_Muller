3b20674618578ea5934ea14a440c19b334a8e436
raph126 <simon.mirkovitch@heig-vd.ch>
2019-05-04 16:18:34 +0200

    add createMultiPlayer in PlayerTest

--- src/test/java/MonopolyGameTest.java
+++ src/test/java/MonopolyGameTest.java
@@ -1,11 +1,14 @@
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class MonopolyGameTest {
     @Test
     public void aPlayerIsWellCreatedByTheGame() {
-        MonopolyGame game = new MonopolyGame(4, 20);
+        MonopolyGame game = new MonopolyGame(1, 20);
         assertNotNull(game.getPlayers().get(0).getBoard());
         assertNotNull(game.getPlayers().get(0).getDice());
         assertNotNull(game.getPlayers().get(0).getPiece());
@@ -13,8 +16,27 @@ public class MonopolyGameTest {
 
     @Test
     public void aPlayerHasTwoDice() {
-        MonopolyGame game = new MonopolyGame(4, 20);
+        MonopolyGame game = new MonopolyGame(1, 20);
         assertEquals(2, game.getPlayers().get(0).getDice().size());
+    }
+
+    @Test
+    public void createMultiPlayer(){
+        String arrayExpected[] = new String[4];
+        arrayExpected[0] = "Player0";
+        arrayExpected[1] = "Player1";
+        arrayExpected[2] = "Player2";
+        arrayExpected[3] = "Player3";
+
+        MonopolyGame game = new MonopolyGame(4, 20);
+
+        String arrayCreated[] = new String[4];
+
+        for(int i = 0; i < 4; ++i) {
+            arrayCreated[i] = game.getPlayers().get(i).getName();
+        }
+
+        assertArrayEquals(arrayExpected, arrayCreated);
     }
 
     @Test
